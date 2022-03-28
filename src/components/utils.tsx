5f3f72a79036bb9f6fc835f1be408bacc412d8d2
Bibi Kuhn <gbr.kuhn@gmail.com>
2022-03-28 21:02:11 +0200

    added comments

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -11,21 +11,27 @@ export const handleCardclick = (
   id: number,
   base_experience: number
 ) => {
+  
   const element = event.target as HTMLElement;
+  
+  // handling styles
   if (element.classList.length === 4) {
     element.classList.add("bg-slate-600");
   } else {
     element.classList.remove("bg-slate-600");
     element.classList.add("bg-slate-200");
   }
-  handlingExpPoints(id, base_experience, element.id);
+
+  handlingBoards(id, base_experience, element.id);
 };
 
-const handlingExpPoints = async (
+// creating one array for each board with selected elements
+const handlingBoards = async (
   id: number,
   base_experience: number,
   boardId: string
 ) => {
+
   let data = {
     id: id,
     exp: base_experience,
@@ -38,43 +44,44 @@ const handlingExpPoints = async (
     case "boardOne":
       inventory = cardsBoardOne;
       break;
-
     case "boardTwo":
       inventory = cardsBoardTwo;
       break;
   }
 
   if (inventory.length === 0) {
-    inventory.push(data);
+    inventory.push(data); // allways pushing first element clicked
   } else {
     let pushData = false;
+
     await inventory.forEach((element, i) => {
-      if (element.id === id) {
+      if (element.id === id) { // checking if element is already at the board, if so the click means "remove it from the board"
         pushData = false;
-        inventory.splice(i, 1);
+        inventory.splice(i, 1); // removing element from the board array
       } else {
-        pushData = true;
+        pushData = true; // pushing element for the board array
       }
     });
+
     if (pushData) inventory.push(data);
   }
 };
+
 
 export const checkingExchange = () => {
   let sumBoardOne = 0;
   let sumBoardTwo = 0;
   const tol = 30;
 
+  // getting the sum of base_experience in each board array to compare
   cardsBoardOne.forEach((element) => {
-    sumBoardOne = sumBoardOne + element.exp;
+    sumBoardOne = sumBoardOne + element.exp; 
   });
-
   cardsBoardTwo.forEach((element) => {
     sumBoardTwo = sumBoardTwo + element.exp;
   });
 
-  const rest = Math.abs(sumBoardOne - sumBoardTwo);
-
+  const rest = Math.abs(sumBoardOne - sumBoardTwo); 
   let msg;
 
   if (rest <= tol) {
